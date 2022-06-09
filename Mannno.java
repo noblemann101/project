2866f407bc5d1e1d8ba0234acce7f36a31f0615c
Noble Mann <106340519+noblemann101@users.noreply.github.com>
2022-06-09 08:18:06 -0700

    Add files via upload

---
+++ Mannno.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mannno;
+
+/**
+ *
+ * @author HP
+ */
+public class Mannno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double[] price = new double[3];
+        for (double prices : price) {
+            System.out.println(prices);
+        }
+    }
+    
+}
